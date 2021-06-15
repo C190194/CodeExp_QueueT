c64477640d98e158a844f1098507778d33e38cc6
kaikiat <pohkaikiat98@gmail.com>
2021-06-15 22:18:34 +0800

    fix: rebase project
    BREAKING CHANGE:

--- App.js
+++ App.js
@@ -1,34 +1,80 @@
-import React from 'react'
-import { Provider } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { theme } from './src/core/theme'
 import {
   StartScreen,
   LoginScreen,
   RegisterScreen,
   ResetPasswordScreen,
-  Dashboard,
+  MainScreen,
+  QueueScreen,
+  ProfileScreen,
+  FavouriteScreen,
 } from './src/screens'
 import AdminQueueStack from "./screens/AdminQueueStack";
 import NextNumberScreen from "./screens/NextNumberScreen";
+import { theme } from './src/core/theme'
+import { Provider } from 'react-native-paper'
+import React from 'react';
+import { Button, View, Text,Alert ,SafeAreaView } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer ,DrawerActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator} from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
 
-const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+     let iconName;
+     if (route.name === 'Explore') {
+        iconName = focused
+        ? 'ios-add'
+        : 'ios-add-outline';
+      } else if (route.name === 'Queue') {
+        iconName = focused
+        ? 'people-circle'
+        : 'people-circle-outline';
+      }
+      else if (route.name === 'Profile') {
+        iconName = focused
+        ? 'person'
+        : 'person-outline';
+      }
+return <Ionicons name={iconName} size={size} color={color}/>;
+        },
+      })}
+      tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+      }}
+    >
+        <Tab.Screen name="Explore" component={MainScreen} />
+        <Tab.Screen name="Queue"  children={() => <QueueScreen navigation={navigation} />} />
+        <Tab.Screen name="Profile" children={() => <ProfileScreen navigation={navigation} />} >
+        </Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
 
 export default function App() {
   return (
     <Provider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="StartScreen"
+    <NavigationContainer>
+      <Stack.Navigator 
           screenOptions={{
+            headerBackTitleVisible: false,
             headerShown: false,
           }}
-        >
+          >
           <Stack.Screen name="StartScreen" component={StartScreen} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen name="AppDrawer" component={AppDrawer}/>
           <Stack.Screen
             name="ResetPasswordScreen"
             component={ResetPasswordScreen}
@@ -36,7 +82,20 @@ export default function App() {
           <Stack.Screen name="Admin Queue Stack" component={AdminQueueStack} />
           <Stack.Screen name="Next Number Screen" component={NextNumberScreen} />
         </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
     </Provider>
+  );
+}
+
+// @TODO logout 
+// https://stackoverflow.com/questions/61331840/how-do-i-add-a-log-out-button-to-my-drawer-using-react-navigation-v5
+function AppDrawer({navigation}) {
+  return (
+      <Drawer.Navigator initialRouteName="HomeScreen">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Favourites" component={FavouriteScreen} />
+        {/* <Drawer.Screen name="Logout" component={() } /> */}
+      </Drawer.Navigator>
   )
 }
+
